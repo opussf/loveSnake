bcb37dbd44190220f6df2e3938676880ce211ff5
opussf <github@zz9-za.com>
2024-02-24 16:55:52 -1000

    It moves....

--- src/main.lua
+++ src/main.lua
@@ -1,67 +1,46 @@
-local socket=require("socket")
+-- local socket=require("socket")
 
-local now = {}
-local hour, minute, second = 0, 0, 0
 local width, height = 0, 0
-local centerX, centerY = 0, 0
-local fractalLoop = 1
-local handLength = {["s"] = 100, ["m"] = 75, ["h"] = 50}
-local use24 = false
-
-function valueToCoords( myCenterX, myCenterY, value, scale, hourType )
-    -- convert to rad ( based on 1-60 )
-    if hourType and hourType == 12 or hourType == 24 then
-        conversion = (2 * math.pi) / hourType
-        value = value >= hourType and value-hourType or value
-    else
-        conversion = (2 * math.pi) / 60
-    end
-    newValue = (value * conversion) - math.pi/2
-    return myCenterX,myCenterY, myCenterX+(math.cos( newValue ) * scale),myCenterY+(math.sin( newValue ) * scale)
-end
+local maxX, maxY = 0, 0
+local snake = {{1,1}, {2,1}, {2,2}}
+local snakeDirection = {1, 0}
+local snakeSize = 10
+local snakeSpeed = 1 -- size per second
+local updateTime = 0
 
 function love.load()
     -- load function
     width, height = love.graphics.getDimensions()
-    centerX = width/2
-    centerY = height/2
+    maxX = width / snakeSize
+    maxY = height / snakeSize
+    start = os.time()
+    print( maxX, maxY, start )
 end
 
 function love.update( dt )
-    print( dt )
-    now = os.date( "*t" )
-    ms = socket.gettime()
-    second = now.sec + (ms - math.floor(ms))
-    minute = now.min + second/60
-    hour = now.hour + minute/60
+    updateTime = updateTime + dt
+    if updateTime > 1 then
+        updateSnake()
+        updateTime = 0
+    end
+    --print( dt )
+    -- now = os.date( "*t" )
+    -- ms = socket.gettime()
 end
 
 function love.draw()
-    love.graphics.print( string.format( "%i:%i.%i", hour, minute, second ), 10, 10 )
-    love.graphics.circle( 'line', centerX, centerY, handLength.s )
-    centers = {centerX, centerY}
-    newCenters = {}
-    for lcv = 0,fractalLoop-1 do
-        for i = 1,#centers,2 do 
-            cX = centers[i]
-            cY = centers[i+1]
-            --print( cX, cY, #centers )
-            for _,v in ipairs({drawClock( cX, cY, second*lcv )}) do
-                table.insert( newCenters, v )
-                --print(i,v,second)
-            end
-        end
-        centers = newCenters
-    end
+    drawSnake()
 end
 
-function drawClock( centerX, centerY, rotation )  -- seconds (1/60)
-    secondCoords = {valueToCoords( centerX, centerY, second+rotation, handLength.s )}
-    minuteCoords = {valueToCoords( centerX, centerY, minute+rotation, handLength.m )}
-    hourCoords = {valueToCoords( centerX, centerY, hour+(rotation/5), handLength.h, use24 and 24 or 12 )}
+function updateSnake()
+    firstSegment = snake[#snake]
+    newHead = {firstSegment[1] + snakeDirection[1], firstSegment[2] + snakeDirection[2]}
+    snake[#snake+1] = newHead
+    table.remove( snake, 1 )
+end
 
-    love.graphics.line( secondCoords )
-    love.graphics.line( minuteCoords )
-    love.graphics.line( hourCoords )
-    return secondCoords[3], secondCoords[4], minuteCoords[3], minuteCoords[4], hourCoords[3], hourCoords[4] 
+function drawSnake( )
+    for _, segment in pairs( snake ) do
+        love.graphics.rectangle( "fill", segment[1]*snakeSize, segment[2]*snakeSize, snakeSize, snakeSize )
+    end
 end
